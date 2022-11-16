2b15a69478c341c5acde477e44b280d293843804
vinaynan1 <vinay.nan@gmail.com>
2022-11-16 12:38:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("It worked");
+    res.send("It worked Welcome to Devops");
 });
 
 const port = 8080;
